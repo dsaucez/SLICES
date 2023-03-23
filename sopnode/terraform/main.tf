fe4c1819bec42943184d9f8cf9f35d4b55e248b8
Damien Saucez <damien.saucez@gmail.com>
2023-03-23 13:18:28 +0000

    bugfix

--- sopnode/terraform/main.tf
+++ sopnode/terraform/main.tf
@@ -40,6 +40,7 @@ resource "google_compute_instance" "compute" {
 
   boot_disk {
     initialize_params {
+      size = 50
       image = "rocky-linux-cloud/rocky-linux-9-v20230306"
 #      image = "ubuntu-os-cloud/ubuntu-2004-lts"
 #      image = "debian-cloud/debian-10"
